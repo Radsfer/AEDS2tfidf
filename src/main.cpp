d9550c0183ad7f6c8c6845ccc3ecba2540f141a7
Jader Oliveira SIlva <145563581+0livas@users.noreply.github.com>
2024-10-31 20:48:35 -0300

    v1.2
    Adição de imagens para demonstrar os resultados

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
 
+  clock_t inicio = clock();
+
   set<string> stopwords;
   vector<string> titulos = {
       "datasets/A mão e a luva.txt",       "datasets/biblia.txt",
@@ -17,14 +19,20 @@ int main() {
     abrirArq(titulos[i], listas[i], stopwords);
   }
 
+  clock_t fim = clock();
+
+  cout << "Tempo de execução = " << (fim - inicio)/CLOCKS_PER_SEC << " segundos.\n" << endl;
+
   char CondicaodeParada;
 
   do {
-    cout << "Digite a sentença buscada: " << endl;
+    cout << "Digite a sentença buscada. " << endl;
     string entrada;
     cout << "Frase desejada: " << entrada << endl;
     getline(cin, entrada);
+    cout << "-----------------" << endl;
     TF_IDF(listas, entrada, stopwords);
+    cout << "-----------------" << endl;
     cout << "\n Deseja buscar outra sentença? (S/N)." << endl;
     cin >> CondicaodeParada;
     cin.ignore();
